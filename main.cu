af488f068b25c0641ed651e71614f775012ebf50
Thomas O'Donnell <todonn2@lsu.edu>
2024-07-13 17:06:05 -0500

    Add video input and display feature

--- main.cu
+++ main.cu
@@ -1,14 +1,41 @@
+#include <stdio.h>
+#include <string>
+
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
-#include <stdio.h>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
 
-__global__ void testKernel() {
-	printf("%s", "Hello from the GPU!\n");
-}
+using namespace cv;
+using namespace std;
 
-int main() {
-	testKernel << <1, 1 >> > ();
-	cudaDeviceSynchronize();
-	printf("%s", "Hello from the CPU!\n");
+const string path = "C:\\Users\\thoma\\source\\repos\\Video-Rendering-Application\\Videos\\Rubix.avi";
+
+
+int main(int argc, char** argv) { 
+	
+	Mat frame;
+	VideoCapture capture(path);
+	
+	if (!capture.isOpened()) {
+		throw invalid_argument("File upload failed.");
+		return -1;
+	}
+
+	namedWindow("Video Input", 1);
+	for (;;)
+	{
+		capture >> frame;
+		if (frame.empty())
+			break;
+		imshow("Video Input", frame);
+		waitKey(20);
+	}
+	waitKey(0);
+	
 	return 0;
 }
+
